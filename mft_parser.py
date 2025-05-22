4488c4d87329efc0a1b6f52c6e399b83eebaca68
Zehra Kolsuz <86024785+zehrakolsuz@users.noreply.github.com>
2025-05-23 00:07:00 +0300

    Create mft_parser.py
    İlk proje kurulumu ve temel dosya yapısı-Initial project setup and basic file structure

---
+++ mft_parser.py
@@ -0,0 +1,8 @@
+# MFT Parser with Python by Zehra Kolsuz
+# Bu script, NTFS dosya sisteminin Master File Table (MFT)'sini ayrıştırır.
+
+def parse_mft(file_path):
+    pass
+
+if __name__ == "__main__":
+    pass
